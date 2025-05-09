951424c7d1941d7d97d4b970e45fdb0a1e9d460a
Nuzler <86511139+Nuzler@users.noreply.github.com>
2025-05-09 19:06:45 +0530

    Update OrderController.java

--- src/main/java/com/drivetrue/DriveTrueApp/controller/OrderController.java
+++ src/main/java/com/drivetrue/DriveTrueApp/controller/OrderController.java
@@ -32,7 +32,7 @@ import com.drivetrue.PayHereHashGenerator;
 
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5173","https://drive-true-front-end.vercel.app/","https://drive-true-dash-bord-3zgw.vercel.app/"})
+@CrossOrigin(origins = {"http://localhost:5173","https://drive-true-front-end.vercel.app/","https://drive-true-dash-bord-hqcx.vercel.app/"})
 public class OrderController {
     
     @Autowired
